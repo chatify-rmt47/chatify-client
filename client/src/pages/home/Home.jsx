29a67bedd8bb493e8db72482fcdd0be208561047
mspadilapadli <msokhibulpadilapadli@gmail.com>
2024-04-23 22:42:36 +0700

    feat: message page templete : done

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-// import MessageContainer from "../../components/messages/MessageContainer";
+import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
             {/* <h1>testing</h1> */}
             <Sidebar />
-            {/* <MessageContainer /> */}
+            <MessageContainer />
         </div>
     );
 };
