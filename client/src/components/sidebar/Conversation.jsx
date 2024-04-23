c4a9edb0b612ae6e4c00d7e6196b0081673fbc73
mspadilapadli <msokhibulpadilapadli@gmail.com>
2024-04-23 22:02:25 +0700

    feat: message on progress

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -16,7 +16,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
                         {/* <img src={conversation.profilePic} alt="user avatar" /> */}
                         <img
                             src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-                            //  src="https://cdne.iconfinder.com/data/icons/communication-line-10/24/ account_profile_user_contact_person_avatar_placeholder-512.png"
+                            //  src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/ account_profile_user_contact_person_avatar_placeholder-512.png"
                             alt="user avatar"
                         />
                     </div>
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,14 @@
 import Conversations from "./Conversations";
-// import LogoutButton from "./LogoutButton";
+import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
     return (
-        //   <div className="border-r border-slate-500 p-4 flex flex-col">
-        <div>
+        <div className="border-r border-slate-500 p-4 flex flex-col">
             <SearchInput />
             <div className="divider px-3"></div>
             <Conversations />
-            {/* <LogoutButton /> */}
+            <LogoutButton />
             <h4>sidebar</h4>
         </div>
     );
