624091e002841e18a25d34bdcc19309dd855ad5b
ghazialfa <ghazialfawwaz01@gmail.com>
2024-04-24 10:20:33 +0700

    feat: auth unfinished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,60 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import { useAuthContext } from "./contexts/AuthContext";
 
-const { authUser } = useAuthContext();
-console.log("🚀 ~ App ~ authUser:", authUser);
-
-const router = createBrowserRouter([
-  {
-    path: "/signup",
-    element: () => {
-      !authUser ? <Signup /> : <Home />;
-    },
-  },
-  {
-    path: "/login",
-    element: () => {
-      !authUser ? <Login /> : <Home />;
-    },
-  },
-  {
-    path: "/",
-    element: () => {
-      authUser ? <Home /> : <Login />;
-    },
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/signup",
+//     element: () => {
+//       !authUser ? <Signup /> : <Home />;
+//     },
+//   },
+//   {
+//     path: "/login",
+//     element: () => {
+//       !authUser ? <Login /> : <Home />;
+//     },
+//   },
+//   {
+//     path: "/",
+//     element: () => {
+//       authUser ? <Home /> : <Login />;
+//     },
+//   },
+// ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  // const { authUser } = useAuthContext();
+  // // console.log("🚀 ~ App ~ authUser:", authUser);
+
+  // return <RouterProvider router={router} />;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
