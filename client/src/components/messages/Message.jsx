b121e1118a11912036cf912810223e3e0bff795b
ghazialfa <ghazialfawwaz01@gmail.com>
2024-04-24 13:50:39 +0700

    feat: setup messages field

---
+++ client/src/components/messages/Message.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src="" /* foto profil */
+          />
+        </div>
+      </div>
+      <div className="">{/* pesan */}</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        {/* waktu */}
+      </div>
+    </div>
+  );
+};
+
+export default Message;
