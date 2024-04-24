28d9a3537a94603115d943229d72110c47997729
Nathaniel Kevin <nathaniel.kevin.untoro@gmail.com>
2024-04-24 19:09:22 +0700

    update: updating the layout and emoji in the application

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -41,47 +41,45 @@
 // export default Conversation;
 
 // STARTER CODE SNIPPET
-import { setSelectedConversation } from "../../store/conversationSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { setSelectedConversation } from '../../store/conversationSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
-    const dispatch = useDispatch();
-    const selectedConversation = useSelector(
-        (state) => state.conversation.selectedConversation
-    );
+  const dispatch = useDispatch();
+  const selectedConversation = useSelector(
+    (state) => state.conversation.selectedConversation
+  );
 
-    const isSelected = selectedConversation?._id === conversation._id;
+  const isSelected = selectedConversation?._id === conversation._id;
 
-    return (
-        <>
-            <div
-                className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-sky-500" : ""}
+  return (
+    <>
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				${isSelected ? 'bg-sky-500' : ''}
 			`}
-                onClick={() => dispatch(setSelectedConversation(conversation))}
-            >
-                <div className="avatar online">
-                    <div className="w-12 rounded-full">
-                        <img
-                            src={conversation.profilePic}
-                            // src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
-                            alt="user avatar"
-                        />
-                    </div>
-                </div>
+        onClick={() => dispatch(setSelectedConversation(conversation))}
+      >
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img
+              src={conversation.profilePic}
+              // src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+              alt="user avatar"
+            />
+          </div>
+        </div>
 
-                <div className="flex flex-col flex-1">
-                    <div className="flex gap-3 justify-between">
-                        <p className="font-bold text-gray-200">
-                            {conversation.fullName}
-                        </p>
-                        <span className="text-xl">🎃</span>
-                    </div>
-                </div>
-            </div>
-            {!lastIdx && <div className="divider my-0 py-0 h-1" />}
-            {/* <div className="divider my-0 py-0 h-1" /> */}
-        </>
-    );
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
+          </div>
+        </div>
+      </div>
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+      {/* <div className="divider my-0 py-0 h-1" /> */}
+    </>
+  );
 };
 export default Conversation;
